e277037e1a58829b5232b7a8d3e6e56b862a3d70
Anthony Renouf <ajrenouf@andrew.cmu.edu>
2023-06-16 12:41:02 -0400

    Update SpawnableObjectMovement.cs

--- Paintballoon/Assets/ExampleAssets/Scripts/SpawnableObjectMovement.cs
+++ Paintballoon/Assets/ExampleAssets/Scripts/SpawnableObjectMovement.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class SpawnableObjectMovement : MonoBehaviour
+
+public float MovementSpeed = 1.0f;
+public float MovementRadius = 1.0f;
+private vector3 initialPosition;
+    
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject objectToFind = GameObject.Find("SpawnableObject");
+        if (objectToFind != null)
+        {
+
+        }
     }
 }
