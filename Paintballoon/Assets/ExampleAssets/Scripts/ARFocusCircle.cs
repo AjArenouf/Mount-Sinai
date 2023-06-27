48b6c4a11e8ed846d2c597569ed15dcea85d76a0
Anthony Renouf <ajrenouf@andrew.cmu.edu>
2023-06-27 12:09:14 -0400

    nexttrackertest

--- Paintballoon/Assets/ExampleAssets/Scripts/ARFocusCircle.cs
+++ Paintballoon/Assets/ExampleAssets/Scripts/ARFocusCircle.cs
@@ -5,16 +5,20 @@ using UnityEngine.XR.ARFoundation;
 using UnityEngine.Experimental.XR;
 using System;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ARFocusCircle : MonoBehaviour
 {
     //public GameObject scanText;
     //public GameObject placeText;
 
-    int objIndex;
+    public GameObject object1;
+    public GameObject object2;
+    public GameObject object3;
+    public GameObject object4;
+    public GameObject object5;
 
-    public GameObject[] virtual_objects;
-    public GameObject[] buttons;
+    public GameObject button;
 
     public GameObject placementIndicator;
     //private GameObject carParent;
@@ -51,94 +55,100 @@ public class ARFocusCircle : MonoBehaviour
 
     public void HideUI()
     {
+        Button[] buttons = FindObjectsOfType<Button>();
 
         if (isUIHidden == false)
         {
-            foreach (var button in buttons)
-            {
-                button.gameObject.SetActive(false);
+                foreach (var button in buttons)
+                {
+                    button.gameObject.SetActive(false);
+                }
+
+                placementIndicatorEnabled = false;
+                placementIndicator.SetActive(false);
+
+                isUIHidden = true;
             }
+            else if (isUIHidden == true)
+            {
+                foreach (var button in buttons)
+                {
+                    button.gameObject.SetActive(true);
+                }
 
-            placementIndicatorEnabled = false;
-            placementIndicator.SetActive(false);
+                placementIndicatorEnabled = true;
+                placementIndicator.SetActive(true);
 
-            isUIHidden = true;
+                isUIHidden = false;
+            }
         }
 
-        else if (isUIHidden == true)
+        public void PlaceObject()
         {
+            GameObject[] virtualObjects = new GameObject[] { object1, object2, object3, object4, object5 };
 
-            foreach (var button in buttons)
+            for (int i = 0; i < virtualObjects.Length; i++)
             {
-                button.gameObject.SetActive(true);
+                GameObject objectToPlace = Instantiate(virtualObjects[i]);
+                objectToPlace.SetActive(true);
+                objectToPlace.transform.position = placementPose.position;
+                objectToPlace.transform.rotation = placementPose.rotation;
             }
-
-            placementIndicatorEnabled = true;
-            placementIndicator.SetActive(true);
-
-            isUIHidden = false;
-
         }
-    }
 
-    public void PlaceObject()
+    public void SpawnAllObjects()
     {
-        for(int i = 0; i < virtual_objects.Length; i++)
-        {
-            GameObject objectToPlace = Instantiate(virtual_objects[i]);
-            objectToPlace.SetActive(true);
-            objectToPlace.transform.position = placementPose.position;
-            objectToPlace.transform.rotation = placementPose.rotation;
-        }
+        PlaceObject();
     }
 
-
-
-    private void UpdatePlacementIndicator()
-    {
-        if (placementPoseIsValid)
+        private void UpdatePlacementIndicator()
         {
-            placementIndicator.SetActive(true);
-
-            foreach (var button in buttons)
+            if (placementPoseIsValid)
             {
-                button.gameObject.SetActive(true);
+                placementIndicator.SetActive(true);
+
+                Button[] buttons = FindObjectsOfType<Button>();
+
+                foreach (var button in buttons)
+                {
+                    button.gameObject.SetActive(true);
+                }
+
+                //scanText.SetActive(false);
+                //placeText.SetActive(true);
+
+                placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
             }
-
-            //scanText.SetActive(false);
-            //placeText.SetActive(true);
-
-            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
-        }
-        else
-        {
-            placementIndicator.SetActive(false);
-
-            foreach (var button in buttons)
+            else
             {
-                button.gameObject.SetActive(false);
+                placementIndicator.SetActive(false);
+
+                Button[] buttons = FindObjectsOfType<Button>();
+
+                foreach (var button in buttons)
+                {
+                    button.gameObject.SetActive(false);
+                }
+
+                //placeText.SetActive(false);
             }
-
-            //placeText.SetActive(false);
-
         }
-    }
 
-    private void UpdatePlacementPose()
-    {
-        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
-        var hits = new List<ARRaycastHit>();
-
-        arOrigin.GetComponent<ARRaycastManager>().Raycast(screenCenter, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneEstimated);
-
-        placementPoseIsValid = hits.Count > 0;
-        if (placementPoseIsValid)
+        private void UpdatePlacementPose()
         {
-            placementPose = hits[0].pose;
+            var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+            var hits = new List<ARRaycastHit>();
 
-            var cameraForward = Camera.current.transform.forward;
-            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
-            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+            arOrigin.GetComponent<ARRaycastManager>().Raycast(screenCenter, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneEstimated);
+
+            placementPoseIsValid = hits.Count > 0;
+            if (placementPoseIsValid)
+            {
+                placementPose = hits[0].pose;
+
+                var cameraForward = Camera.current.transform.forward;
+                var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+                placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+            }
         }
     }
-}
