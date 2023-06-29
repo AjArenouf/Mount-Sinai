1122c2b05de14657aa2ecf31da0ce74e6c288aa8
Anthony Renouf <ajrenouf@andrew.cmu.edu>
2023-06-29 12:35:31 -0400

    Scoretest

--- Paintballoon/Assets/ExampleAssets/Scripts/ShootScript.cs
+++ Paintballoon/Assets/ExampleAssets/Scripts/ShootScript.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShootScript : MonoBehaviour
 
 {
     public GameObject paintballoon;
     public Camera arCamera;
+    int score;
+    public Text scoreText;
 
     private void Update()
     {
@@ -15,12 +18,23 @@ public class ShootScript : MonoBehaviour
             ShootBalloon();
         }
     }
-   public void ShootBalloon()
+    public void ShootBalloon()
     {
         GameObject BalloonInstantiated = Instantiate(paintballoon, arCamera.transform.position, arCamera.transform.rotation);
 
         Vector3 impulse = new Vector3(0.0f, 0.0f, 30.0f);
 
         BalloonInstantiated.GetComponent<Rigidbody>().AddRelativeForce(impulse, ForceMode.Impulse);
+    }
+    void OnTriggerEnter(Collider other)
+
+    {
+        if (other.gameObject.tag == "Spawnable")
+        {
+            score++;
+
+            scoreText.text = "Score:" + score;
+        }
+
     }
 }
