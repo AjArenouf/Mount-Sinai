6549d71aaad7191492b25a5d5277cc1eee21a5da
Anthony Renouf <ajrenouf@andrew.cmu.edu>
2023-07-06 12:27:27 -0400

    RestartGamebutton

---
+++ Paintballoon/Assets/ExampleAssets/Scripts/RestartGame.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            RestartScene();
+        }
+
+
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+}
